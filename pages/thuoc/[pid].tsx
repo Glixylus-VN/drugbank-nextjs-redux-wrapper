3ad8d750fd07e6d5dbd423f8fee6a5946f1e1d8c
Nguyễn Văn Phú Nhàn <pnnguyen9999@outlook.com.vn>
2021-08-03 19:21:31 +0700

    SSR

--- pages/thuoc/[pid].tsx
+++ pages/thuoc/[pid].tsx
@@ -18,63 +18,9 @@ export default function Thuoc({ pid, detailData }: Props): ReactElement {
   return <div>{detailData?.congTyDk}</div>;
 }
 
-// export const getServerSideProps = wrapper.getServerSideProps(
-//   (store) =>
-//     async ({ params }) => {
-//       const { pid } = params;
-
-//       await store.dispatch(getDetailDrug(pid as string));
-//       // console.log("State on server", store.getState());
-//       return {
-//         props: {
-//           pid,
-//           detailData: store.getState().drugData.detailDrug,
-//         },
-//       };
-//     }
-// );
-
-// export async function getServerSideProps(context) {
-//   console.log(context);
-//   const { pid } = context.query;
-//   return {
-//     props: {
-//       pid,
-//     }, // will be passed to the page component as props
-//   };
-// }
-
-interface itemIntf {
-  id: string;
-}
-
-async function getDrugPids() {
-  const res = await axios.get(
-    "https://drugbank.vn/services/drugbank/api/public/thuoc",
-    {
-      params: {
-        size: 25,
-      },
-    }
-  );
-  const data = res.data;
-  return data.map((item: itemIntf) => ({
-    params: {
-      pid: item.id,
-    },
-  }));
-}
-
-export const getStaticPaths = async () => {
-  const paths = await getDrugPids();
-  return {
-    paths: [] && paths,
-    fallback: true,
-  };
-};
-
-export const getStaticProps = async ({ params }) => {
-  const { pid } = params;
+export async function getServerSideProps(context) {
+  console.log(context);
+  const { pid } = context.query;
   const res = await axios.get(
     `https://drugbank.vn/services/drugbank/api/public/thuoc/${pid}`,
     {
@@ -87,50 +33,6 @@ export const getStaticProps = async ({ params }) => {
     props: {
       pid,
       detailData: res.data,
-    },
-    revalidate: 5,
+    }, // will be passed to the page component as props
   };
-};
-
-// export const getStaticProps = wrapper.getStaticProps(
-//   (store) =>
-//     async ({ params }) => {
-//       const { pid } = params;
-
-//       await store.dispatch(getDetailDrug(params?.pid as string));
-//       // console.log("State on server", store.getState());
-//       return {
-//         props: {
-//           pid,
-//           detailData: store.getState().drugData.detailDrug,
-//         },
-//       };
-//     }
-// );
-
-// export async function getStaticPaths() {
-//   return {
-//     paths: [],
-//     fallback: true,
-//   };
-// }
-
-// export async function getStaticProps(ctx: GetStaticPropsContext) {
-//   const { pid } = ctx.params;
-//   console.log(ctx);
-//   try {
-//     return {
-//       props: {
-//         pid,
-//       },
-//       revalidate: 1,
-//     };
-//   } catch (error) {
-//     return {
-//       props: {
-//         pid,
-//       },
-//       revalidate: 1,
-//     };
-//   }
-// }
+}
